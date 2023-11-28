21f6b49142974e474c593e8f6d7caef26d56aa01
eyvind1 <eyvind.osco@ucsp.edu.pe>
2023-11-28 17:10:17 -0500

    class validators

--- src/persons/dto/person.dto.ts
+++ src/persons/dto/person.dto.ts
@@ -1,13 +1,46 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+
 export class CreatePersonDTO {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(10)
     name: string;
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(10)
     address: string;
+    @IsString()
+    @IsNotEmpty()
     role: string;
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(1)
+    @Max(100)
     age: number
 }
 
 export class UpdatePersonDTO {
+    @IsString()
+    @MinLength(3)
+    @MaxLength(10)
+    @IsOptional()
     name?: string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(10)
+    @IsOptional()
     address?: string;
+
+    @IsString()
+    @IsOptional()
     role?: string;
+    
+    @IsNumber()
+    @IsOptional()
+    @Min(1)
+    @Max(100)
     age?: number
 }
